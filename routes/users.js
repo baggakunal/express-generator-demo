a6885fd44864e088d8b183c59818c8c444635214
baggakunal <baggakunal93@gmail.com>
2019-09-17 00:18:16 +0530

    Request API usage

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/:id', function(req, res) {
+  if(req.params && req.params.id != null && !isNaN(Number(req.params.id))){
+    res.send(`User ID is: ${req.params.id}`);
+  } else {
+    res.send('Invalid ID');
+  }
+});
+
 module.exports = router;
