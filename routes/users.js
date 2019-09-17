f974fef2dd86201a96218ddcaf92492a321ed8a8
baggakunal <baggakunal93@gmail.com>
2019-09-17 23:16:47 +0530

    understanding route method of router

--- routes/users.js
+++ routes/users.js
@@ -2,18 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', function (req, res, next) {
+//   res.send('respond with a resource');
+// });
 
-router.get('/:id', function(req, res) {
+router.get('/:id', function (req, res) {
   console.log(`Headers sent? `, res.headersSent); // false
-  if(req.params && req.params.id != null && !isNaN(Number(req.params.id))){
+  if (req.params && req.params.id != null && !isNaN(Number(req.params.id))) {
     res.send(`User ID is: ${req.params.id}`);
   } else {
     res.send('Invalid ID');
   }
   console.log(`Headers sent? `, res.headersSent); // true
 });
+
+router.route('/').all(function (req, res, next) {
+  next();
+}).get(function (req, res, next) {
+  res.send('route matched');
+}).post(function (req, res, next) {
+  res.send('\nroute matched, different verb\n');
+})
 
 module.exports = router;
