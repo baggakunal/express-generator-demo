8a6030e04c1922736571530fde383da2a41ee668
baggakunal <baggakunal93@gmail.com>
2019-09-18 00:53:33 +0530

    Multiple handlers for single path

--- routes/users.js
+++ routes/users.js
@@ -6,15 +6,15 @@ var router = express.Router();
 //   res.send('respond with a resource');
 // });
 
-router.get('/:id', function (req, res) {
-  console.log(`Headers sent? `, res.headersSent); // false
-  if (req.params && req.params.id != null && !isNaN(Number(req.params.id))) {
-    res.send(`User ID is: ${req.params.id}`);
-  } else {
-    res.send('Invalid ID');
-  }
-  console.log(`Headers sent? `, res.headersSent); // true
-});
+// router.get('/:id', function (req, res) {
+//   console.log(`Headers sent? `, res.headersSent); // false
+//   if (req.params && req.params.id != null && !isNaN(Number(req.params.id))) {
+//     res.send(`User ID is: ${req.params.id}`);
+//   } else {
+//     res.send('Invalid ID');
+//   }
+//   console.log(`Headers sent? `, res.headersSent); // true
+// });
 
 router.route('/').all(function (req, res, next) {
   next();
@@ -23,5 +23,31 @@ router.route('/').all(function (req, res, next) {
 }).post(function (req, res, next) {
   res.send('\nroute matched, different verb\n');
 });
+
+
+router.param('user_id', function (req, res, next, id) {
+  req.user = {
+    id: id,
+    name: 'Kunal'
+  };
+  next();
+})
+
+router.route('/:user_id')
+  .all(function (req, res, next) {
+    next();
+  })
+  .get(function (req, res, next) {
+    res.json(req.user);
+  })
+  .put(function (req, res, next) {
+    next(new Error('Invalid method for this path'));
+  })
+  .post(function (req, res, next) {
+    next(new Error('Invalid method for this path'));
+  })
+  .delete(function (req, res, next) {
+    next(new Error('Invalid method for this path'));
+  });
 
 module.exports = router;
