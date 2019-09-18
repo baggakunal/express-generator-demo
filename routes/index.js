eaae83f6f0d3ef183c2c3211da0fe044f3419664
baggakunal <baggakunal93@gmail.com>
2019-09-18 23:48:54 +0530

    passing data to a view

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/faq', function (req, res, next) {
 });
 
 router.get('/about', function (req, res, next) {
-  res.render('about', { title: 'About' });
+  res.render('about', { title: 'About Us', list: ['This', 'is', 'a', 'list'] });
 });
 
 module.exports = router;
